6df5b852ec730e51a7d448cc6ed4f3d79bdf16d1
ilovemarmaris <43924665+ilovemarmaris@users.noreply.github.com>
2018-10-16 00:17:57 +0300

    Update setup_vm2.sh

--- azure_script/setup_vm2.sh
+++ azure_script/setup_vm2.sh
@@ -2,9 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/ilovemarmaris/runit
+git clone https://github.com/ilovemarmaris/runit7
 
-cd runit
+cd runit7
 azure_script/compile_and_config.sh
 
 
