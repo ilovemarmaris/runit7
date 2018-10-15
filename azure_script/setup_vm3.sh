8e8c80e1e2918f382a96e00dc47649075240ab50
ilovemarmaris <43924665+ilovemarmaris@users.noreply.github.com>
2018-10-16 00:17:02 +0300

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -2,9 +2,9 @@
 sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/ilovemarmaris/runit
+git clone https://github.com/ilovemarmaris/runit7
 
-cd runit
+cd runit7
 azure_script/compile_and_config.sh
 
 
