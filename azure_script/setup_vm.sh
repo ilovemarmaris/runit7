1e20fab0c5bfa7e121163c28315d169a41b3c7fd
ilovemarmaris <43924665+ilovemarmaris@users.noreply.github.com>
2018-10-16 00:15:54 +0300

    Update setup_vm.sh

--- azure_script/setup_vm.sh
+++ azure_script/setup_vm.sh
@@ -4,7 +4,7 @@ sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
 git clone https://github.com/ilovemarmaris/runit
 
-cd runit
+cd runit7
 azure_script/compile_and_config.sh
 cd bin
 ./xmr-stak
